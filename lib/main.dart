5ff90e92283129d805f73bacf07a5f09728c3424
athariqk <alib.athariq@gmail.com>
2024-12-03 22:22:40 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
