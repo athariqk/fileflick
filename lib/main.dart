16c83dcb2e82a809579cf47396396560972b9869
Christoffel Daniel Y. Tambunan <christoffeltambunan54@gmail.com>
2024-12-05 11:11:03 +0700

    merge changes from christo and felix

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,294 @@
-// Copyright 2017-2023, Charles Weinberger & Paul DeMarco.
-// All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-import 'dart:async';
-
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:typed_data';
+import 'dart:io' if (dart.library.io) 'dart:io'; // Platform-specific import
 import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus_windows/flutter_blue_plus_windows.dart';
-
-import 'screens/bluetooth_off_screen.dart';
-import 'screens/scan_screen.dart';
+import 'package:desktop_drop/desktop_drop.dart'; // For desktop drag-and-drop
+import 'package:file_picker/file_picker.dart';
 
 void main() {
-  FlutterBluePlus.setLogLevel(LogLevel.verbose, color: true);
-  runApp(const FileFlickApp());
+  runApp(const MyApp());
 }
 
-//
-// This widget shows BluetoothOffScreen or
-// ScanScreen depending on the adapter state
-//
-class FileFlickApp extends StatefulWidget {
-  const FileFlickApp({Key? key}) : super(key: key);
-
-  @override
-  State<FileFlickApp> createState() => _FileFlickAppState();
-}
-
-class _FileFlickAppState extends State<FileFlickApp> {
-  BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
-
-  late StreamSubscription<BluetoothAdapterState> _adapterStateStateSubscription;
-
-  @override
-  void initState() {
-    super.initState();
-    _adapterStateStateSubscription = FlutterBluePlus.adapterState.listen((state) {
-      _adapterState = state;
-      if (mounted) {
-        setState(() {});
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _adapterStateStateSubscription.cancel();
-    super.dispose();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Widget screen = _adapterState == BluetoothAdapterState.on
-        ? const ScanScreen()
-        : BluetoothOffScreen(adapterState: _adapterState);
-
     return MaterialApp(
-      color: Colors.lightBlue,
-      home: screen,
-      navigatorObservers: [BluetoothAdapterStateObserver()],
+      title: 'FileFlick',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const WelcomePage(),
     );
   }
 }
 
-//
-// This observer listens for Bluetooth Off and dismisses the DeviceScreen
-//
-class BluetoothAdapterStateObserver extends NavigatorObserver {
-  StreamSubscription<BluetoothAdapterState>? _adapterStateSubscription;
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
 
   @override
-  void didPush(Route route, Route? previousRoute) {
-    super.didPush(route, previousRoute);
-    if (route.settings.name == '/DeviceScreen') {
-      // Start listening to Bluetooth state changes when a new route is pushed
-      _adapterStateSubscription ??= FlutterBluePlus.adapterState.listen((state) {
-        if (state != BluetoothAdapterState.on) {
-          // Pop the current route if Bluetooth is off
-          navigator?.pop();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Welcome to FileFlick'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const FileTransferPage()),
+            );
+          },
+        child: const Text('Start'),
+        ),
+      ),
+    );
+  }
+}
+
+class FileTransferPage extends StatefulWidget {
+  const FileTransferPage({super.key});
+
+  @override
+  State<FileTransferPage> createState() => _FileTransferPageState();
+}
+
+class _FileTransferPageState extends State<FileTransferPage> {
+  bool _isDragging = false;
+  bool _isClicked = false;  // Flag to track the button click
+  List<String> _fileNames = []; // File names for display
+  List<Uint8List> _fileBytes = []; // File bytes for web compatibility
+
+  // Function to pick files using FilePicker
+  Future<void> _pickFiles() async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        allowMultiple: true,
+        withData: true, // Ensure bytes are available
+      );
+
+      if (result != null) {
+        setState(() {
+          for (var file in result.files) {
+            if (!_fileNames.contains(file.name)) {
+              _fileNames.add(file.name);
+              _fileBytes.add(file.bytes!);
+            }
+          }
+        });
+      }
+    } catch (e) {
+      debugPrint('Error with FilePicker: $e');
+    }
+  }
+
+  // Drag-and-drop handler for desktop
+  void _handleDragDone(DropDoneDetails details) async {
+    for (var file in details.files) {
+      final fileName = file.name;
+
+      Uint8List fileBytes;
+      if (!kIsWeb && file.path != null) {
+        // For mobile/desktop platforms
+        try {
+          fileBytes = await File(file.path!).readAsBytes();
+        } catch (e) {
+          debugPrint('Error reading file bytes for $fileName: $e');
+          continue;
+        }
+      } else {
+        // For web platforms
+        fileBytes = Uint8List(0);
+      }
+
+      setState(() {
+        if (!_fileNames.contains(fileName)) {
+          _fileNames.add(fileName);
+          _fileBytes.add(fileBytes);
         }
       });
     }
   }
 
+  // Function to delete selected file
+  void _deleteFile(int index) {
+    setState(() {
+      if (index >= 0 && index < _fileNames.length && index < _fileBytes.length) {
+        debugPrint('Deleting file: ${_fileNames[index]}');
+        _fileNames.removeAt(index);
+        _fileBytes.removeAt(index);
+      } else {
+        debugPrint('Invalid index for deletion: $index');
+      }
+    });
+  }
+
   @override
-  void didPop(Route route, Route? previousRoute) {
-    super.didPop(route, previousRoute);
-    // Cancel the subscription when the route is popped
-    _adapterStateSubscription?.cancel();
-    _adapterStateSubscription = null;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('File Transfer'),
+      ),
+      body: Center(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  if (_fileNames.isEmpty) ...[
+                    Expanded(
+                      flex: 9,
+                      child: GestureDetector(
+                        onTap: _pickFiles,
+                        child: DropTarget(
+                          onDragEntered: (details) {
+                            setState(() {
+                              _isDragging = true;
+                            });
+                          },
+                          onDragExited: (details) {
+                            setState(() {
+                              _isDragging = false;
+                            });
+                          },
+                          onDragDone: _handleDragDone,
+                          child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: _isDragging
+                                  ? Colors.deepPurple.withOpacity(0.3)
+                                  : Colors.grey[300],
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(color: Colors.deepPurple, width: 2),
+                            ),
+                            child: Center(
+                              child: Text(
+                                _isDragging ? 'Release to Drop' : 'Tap or Drag Files Here',
+                                style: const TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      flex: 1, // 10% of height
+                      child: Container(
+                        width: double.infinity, // Make the width fill the screen
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _isClicked = true; // Set flag to true when clicked
+                            });
+                            Navigator.pop(context); // To go back
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: _isClicked ? Colors.white : Colors.red,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Adjust the radius value here
+                            ),
+                          ),
+                          child: Text(
+                            'Cancel',
+                            style: TextStyle(
+                              color: _isClicked ? Colors.black : Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ] else ...[
+                    Expanded(
+                      flex: 5,
+                      child: ListView.builder(
+                        itemCount: _fileNames.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                            elevation: 5,
+                            child: ListTile(
+                              leading: const Icon(Icons.file_present, color: Colors.deepPurple),
+                              title: Text(_fileNames[index]),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.delete, color: Colors.red),
+                                onPressed: () => _deleteFile(index),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: GestureDetector(
+                        onTap: _pickFiles,
+                        child: DropTarget(
+                          onDragEntered: (details) {
+                            setState(() {
+                              _isDragging = true;
+                            });
+                          },
+                          onDragExited: (details) {
+                            setState(() {
+                              _isDragging = false;
+                            });
+                          },
+                          onDragDone: _handleDragDone,
+                          child: Container(
+                            width: double.infinity,
+                            margin: const EdgeInsets.only(top: 10), // Added margin to the right
+                            decoration: BoxDecoration(
+                              color: _isDragging
+                                  ? Colors.deepPurple.withOpacity(0.3)
+                                  : Colors.grey[300],
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(color: Colors.deepPurple, width: 2),
+                            ),
+                            child: Center(
+                              child: Text(
+                                _isDragging ? 'Release to Drop' : 'Drag Files Here',
+                                style: const TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1, // 10% of height
+                      child: Container(
+                        width: double.infinity,
+                        margin: const EdgeInsets.only(top: 10), // Make the width fill the screen
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context); // To go back
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Adjust the radius value here
+                            ),
+                          ),
+                          child: const Text(
+                            'FLICK!',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import desktop_drop
 import flutter_blue_plus
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
